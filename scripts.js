c7c753d86b006e8c10a68268f8acf1057880cd92
Lubomír Pařil <luboparil@gmail.com>
2020-07-24 15:51:00 +0200

    + css styling

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let fileList = {};
+// 	const fileSelector = document.getElementById('file-selector');
+// 	fileSelector.addEventListener('change', (event) => {
+// 		// const fileList = event.target.files;
+// 		fileList = event.target.files;
+// 		for (const i in fileList) {
+// 			console.log(fileList[i].name);
+// 		}
+		
+// 	});
+
+
+
+
